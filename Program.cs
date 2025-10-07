713733e58467aac172f6ebbbb6138a7df2a86b96
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-10-07 16:08:54 -0300

    Build

--- Program.cs
+++ Program.cs
@@ -8,9 +8,63 @@ namespace GeorgiaDavid_Loops
 {
     internal class Program
     {
+        static bool gameOver;
+
+        static int playerX = 0;
+        static int playerY = 0;
+
         static void Main(string[] args)
         {
+            PlayerDraw(playerX, playerY);
 
+            while(gameOver == false)
+            {
+                PlayerUpdate();
+            }
+
+            void PlayerDraw(int x, int y)
+            {
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(x, y);
+                Console.Write("O");
+            }
+
+            void TrailDraw(int x, int y)
+            {
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(x, y);
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Write(" ");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            void PlayerUpdate()
+            {
+                ConsoleKeyInfo playerInput = Console.ReadKey();
+
+                TrailDraw(playerX, playerY);
+
+                if (playerInput.Key == ConsoleKey.LeftArrow)
+                {
+                    playerX = playerX - 1;
+                    PlayerDraw(playerX, playerY);
+                }
+                else if (playerInput.Key == ConsoleKey.RightArrow)
+                {
+                    playerX = playerX + 1;
+                    PlayerDraw(playerX, playerY);
+                }
+                else if (playerInput.Key == ConsoleKey.UpArrow)
+                {
+                    playerY = playerY - 1;
+                    PlayerDraw(playerX, playerY);
+                }
+                else if (playerInput.Key == ConsoleKey.DownArrow)
+                {
+                    playerY = playerY + 1;
+                    PlayerDraw(playerX, playerY);
+                }
+            }
         }
     }
 }
